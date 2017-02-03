7365a9d694adf43b7e6fd201ab6b8f0eccb51da4
Yang Liu <yang.liu@oculus.com>
2017-02-02 22:22:52 -0800

    make EditorVR work happily with WebVR

---
+++ Assets/EditorVR/Hackathon/WebVRBuilder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+
+#if ENABLE_VR_EDITOR
+public class WebVRBuilder {
+
+    [MenuItem("Oculus/Build WebVR")]
+    public static void MenuItem()
+    {
+        WebVRBuilder builder = new WebVRBuilder();
+        builder.Build();
+    }
+
+    [MenuItem("Oculus/Test")]
+    public static void Test()
+    {
+    }
+
+    public void Build()
+    {
+        string path = EditorUtility.SaveFolderPanel("Choose Location of WebVR Build", "", "");
+        string[] levels = new string[] {"Assets/Scenes/PrototypingTemplate.unity"};
+
+        var options = BuildOptions.Development;
+        BuildPipeline.BuildPlayer(levels, path, BuildTarget.WebGL, options);
+    }
+}
+#endif
