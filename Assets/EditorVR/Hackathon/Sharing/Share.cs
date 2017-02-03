003f8fa07c3abff1078089ae6a94c9ac03ea33c0
Yang Liu <yang.liu@oculus.com>
2017-02-03 00:06:28 -0800

    sharing

--- Assets/EditorVR/Hackathon/Sharing/Share.cs
+++ Assets/EditorVR/Hackathon/Sharing/Share.cs
@@ -9,7 +9,7 @@ public static class Share
 
     public static string SyncFolderToGit(string sourcePath)
     {
-        UnityEngine.Debug.Log("Syncing source folder to git...");
+        UnityEngine.Debug.Log("Syncing source folder " + sourcePath + " to git...");
 
         Process.Start("C:\\Windows\\System32\\cmd.exe", string.Format("/C xcopy /S {0}\\* {1} /e /Y && cd {2} && git add -A && git commit -m \"Sync\" && git push -u origin master", sourcePath, DestinationPath, DestinationPath));
 
--- Assets/EditorVR/Hackathon/WebVRBuilder.cs
+++ Assets/EditorVR/Hackathon/WebVRBuilder.cs
@@ -4,13 +4,27 @@ using System.IO;
 using UnityEditor;
 
 #if ENABLE_VR_EDITOR
-public class WebVRBuilder {
-
+public class WebVRBuilder
+{
     [MenuItem("Oculus/Build WebVR")]
-    public static void MenuItem()
+    public static void BuildMenuItem()
     {
         WebVRBuilder builder = new WebVRBuilder();
         builder.Build();
+    }
+
+    [MenuItem("Oculus/Share WebVR")]
+    public static void ShareMenuItem()
+    {
+        string path = EditorUtility.OpenFolderPanel("Choose Location of WebVR Build", "", "");
+
+        path = path.Replace("/", "\\");
+
+        string sharedPath = Share.SyncFolderToGit(path);
+
+        EditorGUIUtility.systemCopyBuffer = sharedPath;
+
+        Debug.Log(sharedPath + " copied to clipboard.");
     }
 
     [MenuItem("Oculus/Test")]
@@ -21,9 +35,10 @@ public class WebVRBuilder {
     public void Build()
     {
         string path = EditorUtility.SaveFolderPanel("Choose Location of WebVR Build", "", "");
+
         string[] levels = new string[] {"Assets/Scenes/PrototypingTemplate.unity"};
 
-        var options = BuildOptions.Development;
+        var options = BuildOptions.None;
         BuildPipeline.BuildPlayer(levels, path, BuildTarget.WebGL, options);
     }
 }
