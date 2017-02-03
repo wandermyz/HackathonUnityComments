738027032a7dea2faacea2b600a679fa7796f6dc
rift <rift>
2017-02-02 22:34:14 -0800

    Add class to share folder with git

---
+++ Assets/Hackathon/Scripts/Sharing/Share.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public static class Share
+{
+    private const string DestinationPath = "C:\\Users\\Rift\\Projects\\HackathonHost";
+    private const string HostURL = "https://wandermyz.github.io/HackathonHost/";
+
+    public static string SyncFolderToGit(string sourcePath)
+    {
+        UnityEngine.Debug.Log("Syncing source folder to git...");
+
+        Process.Start("C:\\Windows\\System32\\cmd.exe", string.Format("/C xcopy /S {0}\\* {1} /e /Y && cd {2} && git add -A && git commit -m \"Sync\" && git push -u origin master", sourcePath, DestinationPath, DestinationPath));
+
+        UnityEngine.Debug.Log("Sync to git complete!");
+        
+        return HostURL;
+    }
+}
---
+++ Assets/Hackathon/Scripts/Sharing/ShareTester.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShareTester : MonoBehaviour {
+    private const string TestSourcePath = "C:\\Users\\Rift\\test";
+
+    // Use this for initialization
+    void Start () {
+        Share.SyncFolderToGit(TestSourcePath);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+// StringFilePath
